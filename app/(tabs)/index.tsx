f618d479272361728feed7198aa2b43c51fed37c
Banti4750 <bantikumar6203818460@gmail.com>
2025-08-21 11:41:20 +0530

    fix logout btn

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -3,13 +3,15 @@ import { Text, View, } from "react-native";
 import { Button } from "react-native-paper";
 import './../../global.css';
 export default function Index() {
-  const { signOut } = useAuth();
+  const { signOut, user } = useAuth();
   return (
     <View className="flex-1 items-center justify-center bg-white">
       <Text className="text-xl font-bold text-blue-500">
         Welcome to Nativewind!
       </Text>
-      <Button style={{ backgroundColor: 'red' }} mode="text" onPress={signOut} icon={"logout"}>Logout</Button>
+      {user && (<>
+        <Button style={{ backgroundColor: 'red' }} mode="text" onPress={signOut} icon={"logout"}>Logout</Button>
+      </>)}
     </View>
   );
 }
